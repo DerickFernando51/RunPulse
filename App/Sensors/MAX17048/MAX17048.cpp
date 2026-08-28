e3763eabf3ea9f259b9b4098141139f9603e3e02
Derick <derickfernando51@gmail.com>
2026-08-28 14:17:59 +1000

    soc driver fix

--- App/Sensors/MAX17048/MAX17048.cpp
+++ App/Sensors/MAX17048/MAX17048.cpp
@@ -37,36 +37,27 @@ float MAX17048::getVoltage()
     return raw * 0.00125f;
 }
 
-//float MAX17048::getStateOfCharge()
-//{
-//    uint16_t raw =
-//        readRegister(REG_SOC);
-//
-//    // Integer percentage
-//    return raw >> 8;
-//
-//
-//}
+
 
 float MAX17048::getStateOfCharge()
 {
     uint16_t raw = readRegister(REG_SOC);
 
-    char msg[64];
-
-    int len = snprintf(
-        msg,
-        sizeof(msg),
-        "SOC RAW: 0x%04X HIGH: %u LOW: %u\r\n",
-        raw,
-        (raw >> 8) & 0xFF,
-        raw & 0xFF
-    );
-
-    CDC_Transmit_FS(
-        (uint8_t*)msg,
-        len
-    );
+//    char msg[64];
+//
+//    int len = snprintf(
+//        msg,
+//        sizeof(msg),
+//        "SOC RAW: 0x%04X HIGH: %u LOW: %u\r\n",
+//        raw,
+//        (raw >> 8) & 0xFF,
+//        raw & 0xFF
+//    );
+//
+//    CDC_Transmit_FS(
+//        (uint8_t*)msg,
+//        len
+//    );
 
     return (float)((raw >> 8) & 0xFF);
 }
